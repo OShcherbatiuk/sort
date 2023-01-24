855833b278ecc10255204e979020aa881d68fa69
oshcherbatiuk <oleksii.shcherbatiuk@gmail.com>
2023-01-24 18:30:06 +0200

    norm func

--- main.py
+++ main.py
@@ -1,26 +1,62 @@
+import os
+import re
 from pathlib import Path
 
 dir_suff_dict = {"Images": ['.jpg', '.jpeg', '.png', '.gif', '.tiff', '.ico', '.bmp', '.webp', '.svg'],
-                 "Documents": [".pdf", ".txt", ".docx", ".doc", ".ods", ".odt", ".dotx", ".docm", ".dox",
+                 "Documents": [".md", ".epub", ".txt", ".docx", ".doc", ".ods", ".odt", ".dotx", ".docm", ".dox",
                                ".rvg", ".rtf", ".rtfd", ".wpd", ".xls", ".xlsx", ".ppt", ".pptx", ".csv", ".xml"],
                  "Archives": [".iso", ".tar", ".gz", ".7z", ".dmg", ".rar", ".zip"],
                  "Audio": [".aac", ".m4a", ".mp3", "ogg", ".raw", ".wav", ".wma"],
                  "Video": [".avi", ".flv", ".wmv", ".mov", ".mp4", ".webm", ".vob", ".mpg", ".mpeg", ".3gp"],
-                 }
+                 "PDF": [".pdf"],
+                 "HTML": [".html", ".htm", ".xhtml"],
+                 "EXE_MSI": [".exe", ".msi"],
+                 "PYTHON": [".py", ".pyw"]}
 
+
+def normalize(name: str) -> str:
+    CYRILLIC_SYMBOLS = "абвгдеёжзийклмнопрстуфхцчшщъыьэюяєіїґ"
+    TRANSLATION = (
+        "a", "b", "v", "g", "d", "e", "e", "j", "z", "i", "j", "k", "l", "m", "n", "o", "p", "r", "s", "t", "u",
+        "f", "h", "ts", "ch", "sh", "sch", "", "y", "", "e", "yu", "ya", "je", "i", "ji", "g"
+    )
+
+    TRANS = {}
+    for c, l in zip(CYRILLIC_SYMBOLS, TRANSLATION):
+        TRANS[ord(c)] = l
+        TRANS[ord(c.upper())] = l.upper()
+    t_name = name.translate(TRANS)
+    t_name = re.sub(r'\W', '_', t_name)
+    return t_name
 
 
 def sort_func(path_dir):
     cur_dir = Path(path_dir)
-    for file in cur_dir.iterdir():
-        if file.is_dir():
-            if file.stat().st_size == 0:
-                file.rmdir()
-        for suff in dir_suff_dict:
-            if file.suffix.lower() in dir_suff_dict[suff]:
-                dir_img = cur_dir / suff
-                dir_img.mkdir(exist_ok=True)
-                file.rename(dir_img.joinpath(file.name))
+    dir_path = []
+
+    for root, dirs, files in os.walk(path_dir):
+        for d in dirs:
+            dir_path.append(os.path.join(root, d))
+        for file in files:
+            p_file = Path(root) / file
+            name_normalize = f"{normalize(p_file.name[0:-len(p_file.suffix)])}{p_file.suffix}"
+            p_file.rename(Path(root) / name_normalize)
+            p_file = Path(root) / name_normalize
+            for suff in dir_suff_dict:
+                if p_file.suffix.lower() in dir_suff_dict[suff]:
+                    dir_img = cur_dir / suff
+                    dir_img.mkdir(exist_ok=True)
+                    try:
+                        p_file.rename(dir_img.joinpath(p_file.name))
+                    except FileExistsError:
+                        p_file.rename(dir_img.joinpath(f'{p_file.name.split(".")[0]}_c{p_file.suffix}'))
+                        print(f"Возможно дубликат: {p_file.name}")
+
+    for dir_p in reversed(dir_path):
+        if os.path.split(dir_p)[1] in dir_suff_dict or os.stat(dir_p).st_size != 0:
+            continue
+        else:
+            os.rmdir(dir_p)
 
 
 if __name__ == "__main__":
