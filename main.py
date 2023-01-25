9a2539db485f05e0621e8ff8fd291687ca1f30bb
oshcherbatiuk <oleksii.shcherbatiuk@gmail.com>
2023-01-25 13:29:38 +0200

    work with arch

--- main.py
+++ main.py
@@ -1,17 +1,15 @@
 import os
 import re
 from pathlib import Path
+import shutil
 
 dir_suff_dict = {"Images": ['.jpg', '.jpeg', '.png', '.gif', '.tiff', '.ico', '.bmp', '.webp', '.svg'],
-                 "Documents": [".md", ".epub", ".txt", ".docx", ".doc", ".ods", ".odt", ".dotx", ".docm", ".dox",
+                 "Documents": [".md", ".pdf", ".epub", ".txt", ".docx", ".doc", ".ods", ".odt", ".dotx", ".docm",
+                               ".dox",
                                ".rvg", ".rtf", ".rtfd", ".wpd", ".xls", ".xlsx", ".ppt", ".pptx", ".csv", ".xml"],
                  "Archives": [".iso", ".tar", ".gz", ".7z", ".dmg", ".rar", ".zip"],
                  "Audio": [".aac", ".m4a", ".mp3", "ogg", ".raw", ".wav", ".wma"],
-                 "Video": [".avi", ".flv", ".wmv", ".mov", ".mp4", ".webm", ".vob", ".mpg", ".mpeg", ".3gp"],
-                 "PDF": [".pdf"],
-                 "HTML": [".html", ".htm", ".xhtml"],
-                 "EXE_MSI": [".exe", ".msi"],
-                 "PYTHON": [".py", ".pyw"]}
+                 "Video": [".avi", ".flv", ".wmv", ".mov", ".mp4", ".webm", ".vob", ".mpg", ".mpeg", ".3gp"]}
 
 
 def normalize(name: str) -> str:
@@ -30,33 +28,40 @@ def normalize(name: str) -> str:
     return t_name
 
 
-def sort_func(path_dir):
-    cur_dir = Path(path_dir)
+def sort_file(path):
+    cur_dir = Path(path)
     dir_path = []
 
-    for root, dirs, files in os.walk(path_dir):
-        for d in dirs:
-            dir_path.append(os.path.join(root, d))
+    for root, dirs, files in os.walk(path):
+        for item in dirs:
+            dir_path.append(Path(root) / item)
         for file in files:
-            p_file = Path(root) / file
-            name_normalize = f"{normalize(p_file.name[0:-len(p_file.suffix)])}{p_file.suffix}"
-            p_file.rename(Path(root) / name_normalize)
-            p_file = Path(root) / name_normalize
+            file_path = Path(root) / file
+            name_normalize = f"{normalize(file_path.name[0:-len(file_path.suffix)])}{file_path.suffix}"
+            file_path.rename(Path(root) / name_normalize)
+            file_path = Path(root) / name_normalize
             for suff in dir_suff_dict:
-                if p_file.suffix.lower() in dir_suff_dict[suff]:
-                    dir_img = cur_dir / suff
-                    dir_img.mkdir(exist_ok=True)
+                if file_path.suffix.lower() in dir_suff_dict[suff]:
+                    (cur_dir / "move_files").mkdir(exist_ok=True)
+                    dir_move = cur_dir / "move_files" / suff
+                    dir_move.mkdir(exist_ok=True)
                     try:
-                        p_file.rename(dir_img.joinpath(p_file.name))
+                        if file_path.suffix.lower() in [".tar", ".gztar", ".bztar", ".xztar", ".zip"]:
+                            try:
+                                shutil.unpack_archive(file_path, dir_move)
+                                Path(file_path).unlink()
+                            except ValueError:
+                                print("Формат для распаковки не зарегистрирован")
+                                continue
+                        else:
+                            shutil.move(file_path, dir_move)
                     except FileExistsError:
-                        p_file.rename(dir_img.joinpath(f'{p_file.name.split(".")[0]}_c{p_file.suffix}'))
-                        print(f"Возможно дубликат: {p_file.name}")
+                        file_path = f'{file_path.parent / file_path.name.split(file_path.suffix)[0]}_c{file_path.suffix}'
+                        shutil.move(file_path, dir_move / file_path)
+                        print(f"Duplicate?: {file_path}")
 
     for dir_p in reversed(dir_path):
-        if os.path.split(dir_p)[1] in dir_suff_dict or os.stat(dir_p).st_size != 0:
-            continue
-        else:
-            os.rmdir(dir_p)
+        os.rmdir(dir_p)
 
 
 if __name__ == "__main__":
@@ -64,5 +69,5 @@ if __name__ == "__main__":
     if not Path(path_d).exists():
         print('[-] Not found')
     else:
-        sort_func(path_d)
+        sort_file(path_d)
     print('[!] Finish')
