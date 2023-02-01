a0a50b3d0925c12f293ce308912718d8f354ba2c
oshcherbatiuk <oleksii.shcherbatiuk@gmail.com>
2023-02-01 10:27:56 +0200

    upd

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 from setuptools import setup, find_namespace_packages
+
 setup(name='clean_folder',
       version='1',
       description='Clean',
@@ -6,5 +7,6 @@ setup(name='clean_folder',
       author='Oleksii Shcherbatiuk',
       author_email='oleksii.shcherbatiuk@gmail.com',
       license='MIT',
-      packages=find_namespace_packages()
-      entry_points = {'console_scripts': ['clean-folder=clean-folder.clean:main']})
+      packages=find_namespace_packages(),
+      entry_points={'console_scripts': ['clean-folder=clean_folder.clean_folder:main']}
+      )
